6ae16e278ee99458453933a715b5205814fe8411
Kevin Martin <kvn.p.mrtn@gmail.com>
2012-11-07 17:46:56 -0600

    more js stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require modernizr.custom.69142.js
+//= require jquery-1.7.2.min.js
